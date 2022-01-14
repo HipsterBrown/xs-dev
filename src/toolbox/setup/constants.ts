031d8218e2dfe83d34c00188b56dbcf456d50e4c
HipsterBrown <headhipster@hipsterbrown.com>
2022-01-14 18:51:21 -0500

    feat: add esp8266 setup

--- src/toolbox/setup/constants.ts
+++ src/toolbox/setup/constants.ts
@@ -1,11 +1,11 @@
-import { filesystem, system } from 'gluegun'
+import { filesystem } from 'gluegun'
 
 export const HOME_DIR = filesystem.homedir()
 export const INSTALL_DIR = filesystem.resolve(HOME_DIR, '.local', 'share')
 export const INSTALL_PATH =
   process.env.MODDABLE ?? filesystem.resolve(INSTALL_DIR, 'moddable')
-export const PROFILE = await (async function () {
-  const shell = process.env.SHELL ?? (await system.run(`echo $0`))
+export const PROFILE = (function () {
+  const shell = process.env.SHELL ?? ''
   if (shell.includes('zsh')) return '.zshrc'
   if (shell.includes('bash')) return '.bashrc'
   return '.profile'
