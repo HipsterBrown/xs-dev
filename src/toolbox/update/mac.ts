1e7d138b86f81132d9ec9ac658f41619e3af450f
HipsterBrown <headhipster@hipsterbrown.com>
2022-02-16 18:42:26 -0500

    feat(update): add esp32 support, use v4.4 branch of esp-idf

--- src/toolbox/update/mac.ts
+++ src/toolbox/update/mac.ts
@@ -1,7 +1,16 @@
 import { print, system, filesystem } from 'gluegun'
+import { moddableExists } from '../setup/moddable'
 
 export default async function (): Promise<void> {
   print.info('Checking for SDK changes')
+
+  // 0. ensure Moddable exists
+  if (!moddableExists()) {
+    print.error(
+      'Moddable tooling required. Run `xs-dev setup` before trying again.'
+    )
+    process.exit(1)
+  }
 
   const currentRev: string = await system.exec('git rev-parse public', {
     cwd: process.env.MODDABLE,
@@ -23,7 +32,14 @@ export default async function (): Promise<void> {
   await system.exec('git stash', { cwd: process.env.MODDABLE })
   await system.exec('git pull origin public', { cwd: process.env.MODDABLE })
 
-  await system.exec('rm -rf build/{tmp,bin}', { cwd: process.env.MODDABLE })
+  const BUILD_DIR = filesystem.resolve(
+    process.env.MODDABLE ?? '',
+    'build',
+    'bin'
+  )
+  const TMP_DIR = filesystem.resolve(process.env.MODDABLE ?? '', 'build', 'tmp')
+  filesystem.remove(BUILD_DIR)
+  filesystem.remove(TMP_DIR)
   spinner.succeed()
 
   spinner.start('Rebuilding platform tools')
