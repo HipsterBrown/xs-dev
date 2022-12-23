f3340b39258bad1232f4f13f46653aca56bba958
HipsterBrown <headhipster@hipsterbrown.com>
2022-12-23 15:20:22 -0500

    feat: add doctor command to help with environment logging

---
+++ src/toolbox/system/python.ts
@@ -0,0 +1,13 @@
+import { system } from "gluegun";
+
+export function detectPython(): string | null {
+  if (system.which('python') !== null) return 'python'
+  if (system.which('python3') !== null) return 'python3'
+  return null
+}
+
+export async function getPythonVersion(): Promise<string | null> {
+  const python = detectPython()
+  if (python !== null) return (await system.run(`${python} --version`)).split(' ').pop()?.trim() ?? null
+  return null
+}
