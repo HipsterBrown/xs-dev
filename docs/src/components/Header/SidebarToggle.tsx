efbac68b44b08257fd529462ac725234d8020105
Nick Hehr <headhipster@hipsterbrown.com>
2023-03-23 20:04:56 -0400

    chore(docs): migrate preact components to solid-js (#116)
    * chore: upgrade astro, plugins
    
    * chore(docs): migrate preact components to solid-js

--- docs/src/components/Header/SidebarToggle.tsx
+++ docs/src/components/Header/SidebarToggle.tsx
@@ -1,44 +1,43 @@
-import type { FunctionalComponent } from 'preact';
-import { h, Fragment } from 'preact';
-import { useState, useEffect } from 'preact/hooks';
+import { type Component, createSignal, createEffect } from 'solid-js';
 
-const MenuToggle: FunctionalComponent = () => {
-	const [sidebarShown, setSidebarShown] = useState(false);
+const MenuToggle: Component = () => {
+  const [sidebarShown, setSidebarShown] = createSignal(false);
 
-	useEffect(() => {
-		const body = document.getElementsByTagName('body')[0];
-		if (sidebarShown) {
-			body.classList.add('mobile-sidebar-toggle');
-		} else {
-			body.classList.remove('mobile-sidebar-toggle');
-		}
-	}, [sidebarShown]);
+  createEffect(() => {
+    const [body] = document.getElementsByTagName('body');
+    const showSidebar = sidebarShown();
+    if (showSidebar) {
+      body.classList.add('mobile-sidebar-toggle');
+    } else {
+      body.classList.remove('mobile-sidebar-toggle');
+    }
+  });
 
-	return (
-		<button
-			type="button"
-			aria-pressed={sidebarShown ? 'true' : 'false'}
-			id="menu-toggle"
-			onClick={() => setSidebarShown(!sidebarShown)}
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="1em"
-				height="1em"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M4 6h16M4 12h16M4 18h16"
-				/>
-			</svg>
-			<span className="sr-only">Toggle sidebar</span>
-		</button>
-	);
+  return (
+    <button
+      type="button"
+      aria-pressed={sidebarShown() ? 'true' : 'false'}
+      id="menu-toggle"
+      onClick={() => setSidebarShown(current => !current)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+      <span class="sr-only">Toggle sidebar</span>
+    </button>
+  );
 };
 
 export default MenuToggle;
--- docs/src/components/RightSidebar/TableOfContents.tsx
+++ docs/src/components/RightSidebar/TableOfContents.tsx
@@ -1,39 +1,36 @@
-import type { FunctionalComponent } from 'preact'
-import { h, Fragment } from 'preact'
-import { useState, useEffect, useRef } from 'preact/hooks'
+import { type Component, createSignal, onMount, onCleanup } from 'solid-js';
 import type { MarkdownHeading } from 'astro'
 
-const TableOfContents: FunctionalComponent<{ headings: MarkdownHeading[] }> = ({
+const TableOfContents: Component<{ headings: MarkdownHeading[] }> = ({
   headings = [],
 }) => {
-  const itemOffsets = useRef([])
-  const [activeId] = useState<string>(undefined)
-
-  useEffect(() => {
-    const getItemOffsets = () => {
-      const titles = document.querySelectorAll('article :is(h1, h2, h3, h4)')
-      itemOffsets.current = Array.from(titles).map((title) => ({
-        id: title.id,
-        topOffset: title.getBoundingClientRect().top + window.scrollY,
-      }))
-    }
-
+  let itemOffsets = []
+  const [activeId] = createSignal<string>(undefined)
+  const getItemOffsets = () => {
+    const titles = document.querySelectorAll('article :is(h1, h2, h3, h4)')
+    itemOffsets = Array.from(titles).map((title) => ({
+      id: title.id,
+      topOffset: title.getBoundingClientRect().top + window.scrollY,
+    }))
+  }
+  onMount(() => {
     getItemOffsets()
     window.addEventListener('resize', getItemOffsets)
+  })
 
-    return () => {
+  onCleanup(() => {
+    if (typeof window !== 'undefined' && 'removeEventListener' in window) {
       window.removeEventListener('resize', getItemOffsets)
     }
-  }, [])
+  })
 
   return (
     <>
       <h2 class="heading">On this page</h2>
       <ul>
         <li
-          class={`header-link depth-2 ${
-            activeId === 'overview' ? 'active' : ''
-          }`.trim()}
+          class={`header-link depth-2 ${activeId() === 'overview' ? 'active' : ''
+            }`.trim()}
         >
           <a href="#overview">Overview</a>
         </li>
@@ -41,9 +38,8 @@ const TableOfContents: FunctionalComponent<{ headings: MarkdownHeading[] }> = ({
           .filter(({ depth }) => depth > 1 && depth < 4)
           .map((header) => (
             <li
-              class={`header-link depth-${header.depth} ${
-                activeId === header.slug ? 'active' : ''
-              }`.trim()}
+              class={`header-link depth-${header.depth} ${activeId() === header.slug ? 'active' : ''
+                }`.trim()}
             >
               <a href={`#${header.slug}`}>{header.text}</a>
             </li>
