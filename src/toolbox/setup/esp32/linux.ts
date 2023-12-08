357d7e04ca9b2e0e00d645a390d774f71be5efbf
Gustavo <46197948+intGus@users.noreply.github.com>
2023-12-08 15:35:25 -0500

    fix(setup/esp32): update a package name in linux.ts (#152)
    Changed the package name from python-setuptools to python3-setuptools

--- src/toolbox/setup/esp32/linux.ts
+++ src/toolbox/setup/esp32/linux.ts
@@ -6,7 +6,7 @@ export async function installDeps(
   spinner: ReturnType<GluegunPrint['spin']>
 ): Promise<void> {
   await execWithSudo(
-    'apt-get install --yes git wget flex bison gperf python-is-python3 python3-pip python3-serial python-setuptools cmake ninja-build ccache libffi-dev libssl-dev dfu-util',
+    'apt-get install --yes git wget flex bison gperf python-is-python3 python3-pip python3-serial python3-setuptools cmake ninja-build ccache libffi-dev libssl-dev dfu-util',
     { stdout: process.stdout }
   )
   spinner.succeed()
