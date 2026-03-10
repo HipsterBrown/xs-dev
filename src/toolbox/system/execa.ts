ced54f0221c4241e9a159f9bed061c3b17306963
HipsterBrown <headhipster@hipsterbrown.com>
2026-03-10 09:33:55 -0400

    refactor(execa): remove CJS workaround, use static ESM imports

--- src/toolbox/system/execa.ts
+++ src/toolbox/system/execa.ts
@@ -1,17 +1,10 @@
-// Dynamic import wrapper for execa (ESM-only package)
-// Project is not yet migrated to ESM, so static imports of execa fail in CJS context.
-// See: https://github.com/sindresorhus/execa/issues/1159
-
+import { execa as execaFn, execaCommand as execaCommandFn } from 'execa'
 import type { Options, Result } from 'execa'
 
-const execaPromise = import('execa')
-
 export async function execaCommand(command: string, options?: Options): Promise<Result> {
-  const { execaCommand: fn } = await execaPromise
-  return await fn(command, options) as Result
+  return await execaCommandFn(command, options) as Result
 }
 
 export async function execa(file: string, args?: string[], options?: Options): Promise<Result> {
-  const { execa: fn } = await execaPromise
-  return await fn(file, args, options) as Result
+  return await execaFn(file, args, options) as Result
 }
